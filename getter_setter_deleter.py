0ddb68966899e1c32ca9c47119eacb06fb2a58c5
Ivan Sushkov <ivan.sushkov91@gmail.com>
2020-10-11 13:29:25 +0300

    deleter method

--- getter_setter_deleter.py
+++ getter_setter_deleter.py
@@ -39,8 +39,46 @@ class C:
         print("setter method called")
         self._x = x
 
+    @x.deleter
+    def x(self):
+        print("deleter method called")
+        del self._x
 
-c1 = C(-15)
-print(c1.x)
+
 c2 = C(15)
 print(c2.x)
+
+
+# c1 = C(-15)
+# print(c1.x)
+
+
+class E:
+
+    def __init__(self, x):
+        self.x = x
+
+    @property
+    def x(self):
+        print("getter method called")
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        if x < 0:
+            self._x = 0
+        else:
+            self._x = x
+
+    @x.deleter
+    def x(self):
+        print("deleter method called")
+        del self._x
+
+
+e2 = E(22)
+print(e2.x)
+e1 = E(-15)
+print(e1.x)
+del e1.x
+print(e1.x)
